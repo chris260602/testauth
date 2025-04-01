5540bd4e55512aedd8736db6a08535d3cdb3a58c
christoper <christoperlim20@gmail.com>
2025-04-01 21:09:15 +0700

    new

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,10 @@
 'use client'
 
 import { SessionProvider } from "next-auth/react"
+import { auth } from "../../lib/auth"
 
-export function AuthProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>
+
+export async function AuthProvider({ children }: { children: React.ReactNode }) {
+  const session = await auth();
+  return <SessionProvider session={session!}>{children}</SessionProvider>
 }
